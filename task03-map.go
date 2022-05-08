0f5a48a4d3aa78db9b1fea20f9da8ec8919b6c87
RavshanovUsmonbek <usmonravshanov@list.ru>
2022-05-08 19:14:56 +0500

    Second solution: map

--- task03-map.go
+++ task03-map.go
@@ -2,10 +2,14 @@ package homework
 
 import "sort"
 
-func sortMapValues(input map[int]string) (result []string) {
-	for _, value := range input {
-		result = append(result, value)
+func SortMapValues(input map[int]string) (result []string) {
+	indices := []int{}
+	for key := range input {
+		indices = append(indices, key)
 	}
-	sort.Strings(result)
+	sort.Ints(indices)
+	for index := range indices {
+		result = append(result, input[index])
+	}
 	return
 }
